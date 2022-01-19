238b02a6f30f4ee2548b7378040b4c815457e5dc
Alexis.G.GOMEZ <82711005+GZ4003@users.noreply.github.com>
2022-01-19 13:32:38 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import { LoginButton } from './Login';
+import { LogoutButton } from './Logout';
+import { Profile } from './profile'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const {isAuthenticated} = useAuth0();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {isAuthenticated ? (
+          <>
+        <LogoutButton />
+        <Profile />
+          </>
+        ) : (
+          <LoginButton />
+        )}  
+      </header>
+    </div>
+  );
+}
+
+export default App;
